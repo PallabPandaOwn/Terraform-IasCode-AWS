f5b0cfdf8fb1d71b248e499184f94db48c89f3a5
Pallb.Panda@gmail.com <Pallab.Panda@ivanti.com>
2020-02-06 12:37:29 +0530

    add output file of aws plan

--- instance.tf
+++ instance.tf
@@ -1,12 +1,15 @@
 provider "aws" {
   shared_credentials_file = "C://Users//pallab.panda//.aws//credentials"
-  profile                 = ""
+  profile                 = "ivanti"
   region                  = "us-east-1"
 }
 
 resource "aws_instance" "TerraformExer" {
-  name          = "Instance-1"
   ami           = "ami-062f7200baf2fa504"
   instance_type = "t2.micro"
-  
+
 }
+output "outputjson" {
+  value = "${aws_instance.TerraformExer}"
+}
+
