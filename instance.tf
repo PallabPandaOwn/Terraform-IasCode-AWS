81c40507e6b0226c8083a6f65e76ec745cfcfbb3
Pallb.Panda@gmail.com <Pallab.Panda@ivanti.com>
2020-02-06 15:24:49 +0530

    adding elastic ip and s3 bucket using terraform

--- instance.tf
+++ instance.tf
@@ -3,12 +3,22 @@ provider "aws" {
   profile                 = "ivanti"
   region                  = "us-east-1"
 }
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.TerraformExer.id
+}
+
+resource "aws_s3_bucket" "s3bucket" {
+  bucket = "Terraform-Bucket"
+}
 
 resource "aws_instance" "TerraformExer" {
   ami           = "ami-062f7200baf2fa504"
   instance_type = "t2.micro"
-
+  depends_on    = [aws_s3_bucket.s3bucket]
 }
+
+
 output "outputjson" {
   value = "${aws_instance.TerraformExer}"
 }
