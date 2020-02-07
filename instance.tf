92c92b43fcc72e61ed064418d575f9744efa918d
Pallab Panda <Pallab.Panda@gmail.com>
2020-02-07 11:26:14 +0530

    Issue fixed

--- instance.tf
+++ instance.tf
@@ -2,6 +2,9 @@ provider "aws" {
   shared_credentials_file = "C://Users//pallab.panda//.aws//credentials"
   profile                 = "ivanti"
   region                  = "us-east-1"
+
+
+
 }
 resource "aws_eip" "ip" {
   vpc      = true
@@ -12,9 +15,15 @@ resource "aws_s3_bucket" "s3bucket" {
   bucket = "terraformbucket2020"
 }
 
+
+
+
 resource "aws_instance" "TerraformExer" {
   ami           = "ami-062f7200baf2fa504"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.TerraformExer.public_ip} > ip_address.txt"
+  }
   depends_on    = [aws_s3_bucket.s3bucket]
 }
 
