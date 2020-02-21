67c4822b09ff559489b318b63b4ff9ab7eab537b
Pallab Panda <Pallab.Panda@gmail.com>
2020-02-21 11:54:26 +0530

    variable added

--- instance.tf
+++ instance.tf
@@ -6,27 +6,38 @@ provider "aws" {
 
 
 }
+#variable declaration
+
+#String type variable
+variable "myvar" {
+  type    = "string"
+  default = "Hello Terraform"
+}
+#Map Type variable
+variable "mymap" {
+  type = map(string)
+  default = { mykey1 = "value1" }
+}
+
+####################################
+
+#resource decaration
 resource "aws_eip" "ip" {
   vpc      = true
   instance = "${aws_instance.TerraformExer.id}"
 }
-
 resource "aws_s3_bucket" "s3bucket" {
   bucket = "terraformbucket2020"
 }
-
-
-
-
 resource "aws_instance" "TerraformExer" {
   ami           = "ami-062f7200baf2fa504"
   instance_type = "t2.micro"
   provisioner "local-exec" {
     command = "echo ${aws_instance.TerraformExer.public_ip} > ip_address.txt"
   }
-  depends_on    = [aws_s3_bucket.s3bucket]
+  depends_on = [aws_s3_bucket.s3bucket]
 }
-
+########################################
 
 output "outputjson" {
   value = "${aws_instance.TerraformExer}"
