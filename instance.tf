94341fda9b1d1ccacf2fd1bd4b5fa1f15f91bc3f
Pallab Panda <Pallab.Panda@gmail.com>
2020-03-09 14:04:41 +0530

    added sricurity group and Nat gateway

--- instance.tf
+++ instance.tf
@@ -1,24 +1,34 @@
 resource "aws_key_pair" "mypubkey" {
   key_name   = "mypubkey.pub"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}" #Read public key content here
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}" # Read public key content here
 }
-resource "aws_instance" "TerraformExer" { # frist instance spinning
+resource "aws_instance" "TerraformExer" { # EC2 Instance creation
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  
+ # the VPC subnet
+  subnet_id = aws_subnet.pallab-test-terraform-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  
   key_name      = "${aws_key_pair.mypubkey.key_name}"
   tags = {
-    Name = "HelloWorld"
+    Name = "TerraformMachine"
   }
-  provisioner "file" {
-    source      = "script.sh"
-    destination = "/tmp/script.sh"
-    connection {
-    host        = "${aws_instance.TerraformExer.public_ip}"
-    type        = "ssh"
-    user        = "${var.INSTANCE_USERNAME}"
-    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
-  }
-  }
+
+
+
+  # provisioner "file" {
+  #   source      = "script.sh"
+  #   destination = "/tmp/script.sh"
+  #   connection {
+  #   host        = "${aws_instance.TerraformExer.public_ip}"
+  #   type        = "ssh"
+  #   user        = "${var.INSTANCE_USERNAME}"
+  #   private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+  # }
+  # }
   #  provisioner "remote-exec" {
   #   inline = [
   #     "chmod +x /tmp/script.sh",
